a7283395c9139f66851833bc70d828c0dc45e48f
Dulan Dhanush <dulandhanush0@gmail.com>
2026-04-07 20:54:09 +0530

    quiz game for lists and other memory storing methods and practice loops

--- first.py
+++ first.py
@@ -1,2 +1,4 @@
+# this is my first python program
+
 print ("Hello World!")
 print ("I am Dulan Dhanush")
---
+++ quizGame.py
@@ -0,0 +1,47 @@
+questions = ("How many elements are in the periodic table?: ", 
+             "Which animal lays the largest eggs?: ", 
+             "What is the most abundant gas in Earth's atmosphere?: ", 
+             "How many bones are in the human body?: ", 
+             "Which planet in the solar system is the hottest?: ")
+
+options = (("A. 116 ", "B. 117 ", "C. 118 ", "D. 119 "),
+           ("A. Whale ", "B. Crocodile ", "C. Elephant ", "D. Ostrich "),
+           ("A. Nitrogen ", "B. Oxygen ", "C. Carbon-Dioxide ", "D. Hydrogen "),
+           ("A. 206 ", "B. 207 ", "C. 208 ", "D. 209 "),
+           ("A. Mercury ", "B. Venus ", "C. Earth ", "D. Mars "))
+
+answers = ("C", "D", "A", "A", "B")
+
+guesses = []
+score = 0
+question_num = 0
+
+for question in questions:
+    print("---------------------")
+    print(question)
+    
+    for option in options[question_num]:
+        print(option)
+
+    guess = input("Enter (A, B, C, D): ").upper()
+    guesses.append(guess)
+
+    if guess == answers[question_num]:
+        print("✅ Correct!")
+        score += 1
+    else:
+        print("❌ Incorrect!")
+        print(f"Correct answer: {answers[question_num]}")
+
+    question_num += 1
+
+
+print("\n---------------------")
+print("      RESULTS        ")
+print("---------------------")
+
+print("Answers: ", " ".join(answers))
+print("Guesses: ", " ".join(guesses))
+
+score_percentage = int((score / len(questions)) * 100)
+print(f"Your score is: {score_percentage}%")
