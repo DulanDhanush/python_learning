0604962e42bea9bd8507d1a9ee56d2dd275654a8
Dulan Dhanush <dulandhanush0@gmail.com>
2026-04-10 14:36:46 +0530

    this console app made to practice functions in python and practice to handle main function

---
+++ banking program.py
@@ -0,0 +1,54 @@
+def show_balance(balance):
+    print(f"Your balance is : {balance:.2f}")
+
+def deposit():
+    amount = float(input("Enter a amount to be deposit: "))
+
+    if amount < 0:
+        print("That is not a valid amount")
+        return 0
+    else:
+        return amount
+
+def withdraw(balance):
+    amount = float(input("Enter amount to be withdraw: "))
+
+    if amount > balance:
+        print("Insufficent funds")
+        return 0
+    elif amount < 0:
+        print("Amount must be greater than 0")
+        return 0
+    else:
+        return amount
+
+def main():
+
+    balance = 0
+    is_running = True
+
+    while is_running:
+        print("Banking Program")
+        print("1.Show balance")
+        print("2.Deposit")
+        print("3.Withdraw")
+        print("4.Exit")
+
+        choice = input("Enter your choice (1-4): ")
+        print()
+
+        if choice == '1':
+            show_balance(balance)
+        elif choice == '2':
+            balance += deposit()
+        elif choice == '3':
+            balance -= withdraw(balance)
+        elif choice == '4':
+            is_running = False
+        else:
+            print("That is not a valid choice")
+
+    print("Thank you have a nice day!")
+
+if __name__ == '__main__':
+    main()
